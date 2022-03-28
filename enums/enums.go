2ef8f9aa99b6dd0a2fb54b80a38c7468a689532d
pnocera <pnocera@gmail.com>
2022-03-28 18:45:54 +0200

    1.3

---
+++ enums/enums.go
@@ -0,0 +1,53 @@
+package enums
+
+//ConnectorSubtype ConnectorSubtype
+type ConnectorSubtype string
+
+const (
+	SharepointConnector     string = "or.co.ob.sh"
+	LocalConnector          string = "or.co.ob.lo"
+	BuilderActionMergeType  string = "ou.ba.mr"
+	DropboxConnector        string = "or.co.ob.db"
+	BuilderActionOutputType string = "ou.ba.op"
+	ConvertNodeType         string = "ou.ba.cv"
+	SetHeadersType          string = "ou.ba.hd"
+	SetFooterType           string = "ou.ba.ft"
+	BookLevelType           string = "ou.bo.lv"
+)
+
+type TaskError string
+
+const (
+	//TaskDownloadError Task Download Error
+	TaskDownloadError = "TaskDownloadError"
+	//TaskProcessError Task Process Error
+	TaskProcessError = "TaskProcessError"
+	//TaskPreProcessError Task PreProcess Error
+	TaskPreProcessError = "TaskPreProcessError"
+	//TaskMergeError Task Merge Error
+	TaskMergeError = "TaskMergeError"
+	//TaskUploadError Task Upload Error
+	TaskUploadError = "TaskUploadError"
+	//TaskUploadError Task Upload Error
+	FinalizeError = "FinalizeError"
+)
+
+type DaprService string
+
+//services names
+const (
+	DataSvc    = "datalayer"
+	NodeapiSvc = "apisvc"
+	Rbacsvc    = "rbacsvc"
+	Compapi    = "compapisvc"
+	Qsvc       = "qsvc"
+)
+
+//actions names
+const (
+	ActionPreprocess  = "preprocess"
+	ActionProcess     = "process"
+	ActionMerge       = "merge"
+	ActionBook        = "book"
+	ActionPostProcess = "postprocess"
+)
