493672fa16192ac16ca19a0188d8ea5e153183a5
pnocera <pnocera@gmail.com>
2022-03-30 18:22:07 +0200

    1.15

--- types/feedbackpayload.go
+++ types/feedbackpayload.go
@@ -1,11 +1,12 @@
 package types
 
 type FeedbackPayload struct {
-	WFID   string       `json:"wfid"`
-	RUNID  string       `json:"runid"`
-	TASKID string       `json:"taskid"`
-	Docs   []*PListItem `json:"docs"`
-	Ok     bool         `json:"ok"`
-	ErrMsg string       `json:"errmsg"`
-	WfStep string       `json:"wfstep"`
+	WFID     string       `json:"wfid"`
+	RUNID    string       `json:"runid"`
+	TASKID   string       `json:"taskid"`
+	Docs     []*PListItem `json:"docs"`
+	Ok       bool         `json:"ok"`
+	ErrMsg   string       `json:"errmsg"`
+	WfStep   string       `json:"wfstep"`
+	WfAction string       `json:"wfaction"`
 }
