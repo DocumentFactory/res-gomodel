80ff36b4ab3b47829eba0a7a0b743c85be9d82ef
pnocera <pnocera@gmail.com>
2022-04-01 18:55:52 +0200

    1.24

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 go mod tidy
 
-git tag v0.1.23
-git push origin v0.1.23
-go list -m github.com/pnocera/res-gomodel@v0.1.23
+git tag v0.1.24
+git push origin v0.1.24
+go list -m github.com/pnocera/res-gomodel@v0.1.24
--- types/user.go
+++ types/user.go
@@ -1,7 +1,16 @@
 package types
 
 type User struct {
-	ID    string `json:"id"`
-	CtxID string `json:"ctxid"`
-	Email string `json:"email"`
+	ID      string      `json:"id"`
+	CtxID   string      `json:"ctxid"`
+	ExtData UserExtdata `json:"extdata"`
+}
+
+type UserExtdata struct {
+	Email      string `json:"email"`
+	Identities []struct {
+		ID       string `json:"id"`
+		Provider string `json:"provider"`
+		Email    string `json:"email,omitempty"`
+	} `json:"identities"`
 }
