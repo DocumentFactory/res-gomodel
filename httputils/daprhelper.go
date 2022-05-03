2092b0787b661670079fe9ba1159b495adc6c511
pnocera <pnocera@gmail.com>
2022-05-03 10:07:29 +0200

    1.40

--- httputils/daprhelper.go
+++ httputils/daprhelper.go
@@ -11,12 +11,15 @@ import (
 	dapr "github.com/dapr/go-sdk/client"
 	"github.com/pnocera/res-gomodel/config"
 	"github.com/pnocera/res-gomodel/enums"
+	"github.com/pnocera/res-gomodel/logs"
 	"github.com/pnocera/res-gomodel/types"
+	"go.uber.org/zap"
 )
 
 type DaprHelper struct {
 	conf   *config.Config
 	client dapr.Client
+	logh   *logs.LogHelper
 }
 
 func NewDaprHelper(conf *config.Config) *DaprHelper {
@@ -29,15 +32,25 @@ func NewDaprHelper(conf *config.Config) *DaprHelper {
 	}
 	c.client = client
 
+	c.logh = logs.NewLogHelper(conf)
+
 	return &c
 }
 
 func (ul *DaprHelper) Get(ctx context.Context, servicename string, api string) ([]byte, error) {
-	return ul.client.InvokeMethod(ctx, servicename, api, "get")
+	response, err := ul.client.InvokeMethod(ctx, servicename, api, "get")
+	if err != nil {
+		ul.logh.Error("Dapr Error getting request ", zap.String("servicename", servicename), zap.String("api", api), zap.String("error", err.Error()))
+	}
+	return response, err
 }
 
 func (ul *DaprHelper) Post(ctx context.Context, servicename string, api string, data interface{}) ([]byte, error) {
-	return ul.client.InvokeMethodWithCustomContent(ctx, servicename, api, "post", "application/json", data)
+	response, err := ul.client.InvokeMethodWithCustomContent(ctx, servicename, api, "post", "application/json", data)
+	if err != nil {
+		ul.logh.Error("Dapr Error posting request ", zap.String("servicename", servicename), zap.String("api", api), zap.String("error", err.Error()))
+	}
+	return response, err
 }
 
 func (ul *DaprHelper) GetExtension(ctx context.Context, Mimetype string) (string, error) {
@@ -45,6 +58,7 @@ func (ul *DaprHelper) GetExtension(ctx context.Context, Mimetype string) (string
 	response, err := ul.client.InvokeMethod(ctx, enums.DataSvc, "mimes/extension/"+strings.ReplaceAll(Mimetype, "/", "|"), "get")
 
 	if err != nil {
+		ul.logh.Error("Dapr Error getting extension ", zap.String("mimetype", Mimetype), zap.String("error", err.Error()))
 		return "", err
 	}
 
@@ -64,6 +78,10 @@ func (ul *DaprHelper) GetSecret(ctx context.Context, nodetype string, secretid s
 	})
 
 	if err != nil {
+		ul.logh.Error("Dapr Error getting secret ",
+			zap.String("nodetype", nodetype),
+			zap.String("secretid", secretid),
+			zap.String("error", err.Error()))
 		return nil, err
 	}
 
@@ -72,11 +90,22 @@ func (ul *DaprHelper) GetSecret(ctx context.Context, nodetype string, secretid s
 	err = json.Unmarshal(secretbytes, &secretresp)
 
 	if err != nil {
+		ul.logh.Error("Dapr Error unmarshalling secret ",
+			zap.String("nodetype", nodetype),
+			zap.String("secretid", secretid),
+			zap.String("bytes", string(secretbytes)),
+			zap.String("error", err.Error()))
 		return nil, err
 	}
 
 	if !secretresp.Ok {
-		return nil, errors.New(secretresp.ErrMsg)
+		err = errors.New(secretresp.ErrMsg)
+		ul.logh.Error("Dapr Error getting secret ",
+			zap.String("nodetype", nodetype),
+			zap.String("secretid", secretid),
+			zap.String("error", err.Error()))
+
+		return nil, err
 	}
 
 	return secretresp.SecretValue, nil
@@ -91,6 +120,11 @@ func (ul *DaprHelper) GetService(ctx context.Context, filename string) (string, 
 
 	response, err := ul.client.InvokeMethod(ctx, enums.DataSvc, "mimes/serviceext/"+ext, "get")
 	if err != nil {
+		ul.logh.Error("Dapr Error getting service ",
+			zap.String("service", enums.DataSvc),
+			zap.String("extension", ext),
+			zap.String("error", err.Error()))
+
 		return "", err
 	}
 
@@ -98,6 +132,11 @@ func (ul *DaprHelper) GetService(ctx context.Context, filename string) (string, 
 
 	err = json.Unmarshal(response, &svcmeta)
 	if err != nil {
+		ul.logh.Error("Dapr Error unmarshalling service ",
+			zap.String("service", enums.DataSvc),
+			zap.String("extension", ext),
+			zap.String("response", string(response)),
+			zap.String("error", err.Error()))
 		return "", err
 	}
 
